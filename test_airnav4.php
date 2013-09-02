7d6de72660bd6bdfee0795f269c9f2c637777ca2
hareluya86 <hareluya86@hotmail.com>
2013-09-03 00:41:46 +0800

    modified:   list/Subscribers_migrated.csv 	modified:   list/Subscribers_migrated.xlsx 	modified:   test_airnav4.php

--- test_airnav4.php
+++ test_airnav4.php
@@ -7,12 +7,52 @@
 
 include("./config.php");
 
+$utable = 'newsletter_subscribers';
+
 // test connection
-$res = mysql_connect($sqlhost);
+$res = mysql_connect($sqlhost,$sqluser,$sqlpass);
 if(!$res){
     echo "Connection fail!";
     exit;
 }
 echo "Connected!";
+mysql_select_db($sqldb,$res);
+
+$csv = fopen('./list/Subscribers_migrated.csv','r');
+$file_index = 1;
+$record_count = 0;
+$batch_size = 1000;
+$sqlcmd = '';
+while(list($id,$uid,$list,$fname,$lname,$email,$user1,$user2,
+        $user3,$user4,$user5,$user6,$user7,$user8,$user9,$user10,
+        $cseq,$cdel,$cnf,$dateadd,$ipaddr,$refurl,$htmail,$bounces) = fgetcsv($csv)){
+    
+    if($file_index++==1) continue;
+    
+    if($record_count%$batch_size == 0){
+        if($sqlcmd){
+            mysql_query($sqlcmd) or die('admin-12-' . mysql_error());
+        }
+        $sqlcmd = "INSERT INTO ".$utable." VALUES ($id,'$uid',$list,'$fname','$lname','$email','$user1','$user2',
+        '$user3','$user4','$user5','$user6','$user7','$user8','$user9','$user10',$cseq,$cdel,'$cnf','$dateadd',
+        '$ipaddr','$refurl','$htmail','$bounces')";
+        echo 'cmd='.$sqlcmd.'<br>';
+        echo 'mod='.$record_count%$batch_size;
+        echo 'write mode';
+    }
+    else{
+        $sqlcmd = $sqlcmd.",($id,'$uid',$list,'$fname','$lname','$email','$user1','$user2',
+        '$user3','$user4','$user5','$user6','$user7','$user8','$user9','$user10',$cseq,$cdel,'$cnf','$dateadd',
+        '$ipaddr','$refurl','$htmail','$bounces')";
+        echo 'cmd='.$sqlcmd.'<br>';
+        echo 'mod='.$record_count%$batch_size;
+        echo 'wait mode';
+    }
+    echo 'count='.$record_count++.'<br>';
+}
+// purge the remaining records 
+if($sqlcmd){
+    mysql_query($sqlcmd) or die('admin-12-' . mysql_error());
+}
 
 ?>
